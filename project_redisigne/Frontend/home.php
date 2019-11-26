ebfd0439f8c731287be3743cce041afb027ae131
MaxCurtis-creater <57647617+MaxCurtis-creater@users.noreply.github.com>
2019-11-25 22:04:51 -0700

    Update home.php

--- project_redisigne/Frontend/home.php
+++ project_redisigne/Frontend/home.php
@@ -4,14 +4,19 @@ include 'home_vars.php';
 session_start();
 if(isset($_SESSION['IsAdmin'])){
     echo "Admin <br>";
-    echo("<form action='home_vars.php' method='POST'>
+    echo("<form action='' method='POST'>
              <label for='p_tag'>P</lable>
              <input type='text' name='p_tag'>
+             <button>change</button>
          </form>");
 }
 //end of session code
-
-
+if($_POST){
+    $p_tag = $_POST['p_tag'];
+}else{
+    $p_tag = "Hello";
+}
+echo $_POST['p_tag'];
 // the pages content 
 echo("<p>".$p_tag."</p>");//ect
 ?>
